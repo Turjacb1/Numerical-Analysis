383d7e666a6941cdcec6a0c45cae97f8a081a3b1
Turja Chakraborty <152412436+Turjacb1@users.noreply.github.com>
2025-01-22 00:25:50 +0600

    Add files via upload

---
+++ LAB1_Bijection_Method.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#define EPSILON 0.01
+
+
+double func(double x)
+{
+	return x*x-7;
+}
+
+
+void bisection(double a, double b)
+{
+	if (func(a) * func(b) >= 0)
+	{
+		printf("You have not assumed right a and b\n");
+		return;
+	}
+
+	double c = a;
+	while ((b-a) >= EPSILON)
+	{
+		
+		c = (a+b)/2;
+
+		
+		if (func(c) == 0.0)
+			break;
+
+		
+		else if (func(c)*func(a) < 0)
+			b = c;
+		else
+			a = c;
+	}
+
+printf("The value of root is :%lf ",c );
+}
+
+
+int main()
+{
+	double a =-1, b = 0;
+	bisection(a, b);
+	return 0;
+}
+
+
+
